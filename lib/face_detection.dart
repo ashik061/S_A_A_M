5b54f23b1c200f5cca36b860ba9bd9fc749b11b9
Borhan20 <borhanuddin.24csedu.020@gmail.com>
2023-12-30 23:01:03 +0600

    nothing important

--- lib/face_detection.dart
+++ lib/face_detection.dart
@@ -1,5 +1,2 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import 'package:s_a_a_m/firebase_options.dart';
-import 'package:s_a_a_m/select_screen.dart';
